0bec38e362c1ff7e3384d87306b0a8ef6e78e3c4
kimyezzang97 <kimyezzang97@gmail.com>
2023-02-14 21:42:24 +0900

    first commit

---
+++ src/main/java/com/vintlyboot/VintlybootApplication.java
@@ -0,0 +1,13 @@
+package com.vintlyboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VintlybootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VintlybootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vintlyboot/VintlybootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vintlyboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VintlybootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
